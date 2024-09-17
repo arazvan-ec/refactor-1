7dfa6065a01d3417fea93607f23cc00c1485407d
Laura Gomez Cabero <lgomez@ext.elconfidencial.com>
2024-09-17 16:52:16 +0200

    legacy orchestrator

--- src/Orchestrator/Chain/LegacyOrchestrator.php
+++ src/Orchestrator/Chain/LegacyOrchestrator.php
@@ -5,18 +5,37 @@
 
 namespace App\Orchestrator\Chain;
 
+use App\Ec\Snaapi\Infrastructure\Client\Http\QueryLegacyClient;
 use Ec\Editorial\Domain\Model\Editorial;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
 
 /**
  * @author Laura Gómez Cabero <lgomez@ext.elconfidencial.com>
  */
 class LegacyOrchestrator implements Orchestrator
 {
+    public function __construct(
+        private readonly QueryLegacyClient $queryLegacyClient
+    ) {
+    }
+
+    /**
+     * @throws Throwable
+     */
     public function execute(Request $request): array
     {
-        // TODO: Implement execute() method.
+        $id = $request->get('id');
+
+        /** @var Editorial|null $editorial */
+        $editorial = $this->queryLegacyClient->findEditorialById($id);
+
+        if (!is_null($editorial)) {
+            return ['editorial' => $editorial];
+        }
+
+        throw new NotFoundHttpException('Editorial data not found for id: '.$id);
     }
 
     public function canOrchestrate(): string
