61ab348d493602e8d04d6256671d453cebd5f103
Laura Gomez Cabero <lgomez@ext.elconfidencial.com>
2024-09-26 13:01:29 +0200

    phpstan and url generator trait test

--- src/Application/DataTransformer/Apps/AppsDataTransformer.php
+++ src/Application/DataTransformer/Apps/AppsDataTransformer.php
@@ -3,7 +3,7 @@
 namespace App\Application\DataTransformer\Apps;
 
 use Ec\Editorial\Domain\Model\Editorial;
-use Ec\Journalist\Domain\Model\Journalists;
+use Ec\Journalist\Domain\Model\Journalist;
 use Ec\Section\Domain\Model\Section;
 
 /**
@@ -11,7 +11,13 @@ use Ec\Section\Domain\Model\Section;
  */
 interface AppsDataTransformer
 {
+    /**
+     * @param Journalist[] $journalists
+     */
     public function write(Editorial $editorial, array $journalists, Section $section): AppsDataTransformer;
 
+    /**
+     * @return Editorial[]
+     */
     public function read(): array;
 }
