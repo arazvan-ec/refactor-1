0b27c2011a01dec6a409908d200c61a51a1ae8b1
jmsantos <jmsantos@elconfidencial.com>
2024-11-12 16:58:23 +0100

    add test Picture

--- tests/Application/DataTransformer/Apps/Body/BodyTagPictureMembershipDataTransformerTest.php
+++ tests/Application/DataTransformer/Apps/Body/BodyTagPictureMembershipDataTransformerTest.php
@@ -2,24 +2,20 @@
 
 namespace App\Tests\Application\DataTransformer\Apps\Body;
 
-use App\Application\DataTransformer\Apps\Body\BodyTagPictureDataTransformer;
+use App\Application\DataTransformer\Apps\Body\BodyTagPictureMembershipDataTransformer;
 use App\Infrastructure\Service\PictureShots;
 use Ec\Editorial\Domain\Model\Body\BodyTagPicture;
-use App\Infrastructure\Service\Thumbor;
 use Ec\Editorial\Domain\Model\Body\BodyTagPictureId;
-use Ec\Multimedia\Domain\Model\Photo\Photo;
-use Ec\Multimedia\Infrastructure\Client\Http\QueryMultimediaClient;
+use Ec\Editorial\Domain\Model\Body\BodyTagPictureMembership;
 use PHPUnit\Framework\TestCase;
 
-class BodyTagPictureDataTransformerTest extends TestCase
+class BodyTagPictureMembershipDataTransformerTest extends TestCase
 {
-    private BodyTagPictureDataTransformer $dataTransformer;
-    private PictureShots $pictureShots;
 
     protected function setUp(): void
     {
         $this->pictureShots = $this->createMock(PictureShots::class);
-        $this->dataTransformer = new BodyTagPictureDataTransformer($this->pictureShots);
+        $this->dataTransformer = new BodyTagPictureMembershipDataTransformer($this->pictureShots);
     }
 
     /**
@@ -37,31 +33,28 @@ class BodyTagPictureDataTransformerTest extends TestCase
             '382w' => 'https://images.ecestaticos.dev/ws_0oo3JORfvWxI_XKyluvDeGRI=/615x99:827x381/382x509/filters:fill(white):format(jpg)/dev.f.elconfidencial.com/original/0a9/783/99c/0a978399c4be84f3ce367624ca9589ad.jpg',
             '328w' => 'https://images.ecestaticos.dev/YsYE5tLIS_WX3BU6agIfeikYUl8=/615x99:827x381/328x437/filters:fill(white):format(jpg)/dev.f.elconfidencial.com/original/0a9/783/99c/0a978399c4be84f3ce367624ca9589ad.jpg',
         ];
-        $caption = 'Sample Caption';
-        $alternate = 'Sample Alternate';
         $orientation = 'landscape';
 
         $this->pictureShots->method('retrieveShotsByPhotoId')->willReturn($shots);
         $bodytagPictureId = $this->createMock(BodyTagPictureId::class);
         $bodytagPictureId->method('id')->willReturn('123');
 
-        $bodyElement = $this->createMock(BodyTagPicture::class);
+        $bodyElement = $this->createMock(BodyTagPictureMembership::class);
         $bodyElement->method('id')->willReturn($bodytagPictureId);
 
-        $bodyElement->method('caption')->willReturn($caption);
-        $bodyElement->method('alternate')->willReturn($alternate);
         $bodyElement->method('orientation')->willReturn($orientation);
 
-        $result = $this->dataTransformer->write($bodyElement)->read();
+        $result = $this->dataTransformer->write($bodyElement,[])->read();
 
         $this->assertEquals($shots, $result['shots']);
-        $this->assertEquals($caption, $result['caption']);
-        $this->assertEquals($alternate, $result['alternate']);
         $this->assertEquals($orientation, $result['orientation']);
     }
 
+    /**
+     * @test
+     */
     public function canTransformShouldReturnBodyTagPictureString(): void
     {
-        $this->assertEquals(BodyTagPicture::class, $this->dataTransformer->canTransform());
+        $this->assertEquals(BodyTagPictureMembership::class, $this->dataTransformer->canTransform());
     }
 }
