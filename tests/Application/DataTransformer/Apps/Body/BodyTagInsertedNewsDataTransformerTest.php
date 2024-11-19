c8043a4ac79dd950b3272190039cb427b25eec10
Ken <kserikawa@ext.elconfidencial.com>
2024-11-19 16:11:54 +0100

    test: update BodyTagInsertedNewsData test and add custom data provider

---
+++ tests/Application/DataTransformer/Apps/Body/BodyTagInsertedNewsDataTransformerTest.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @copyright
+ */
+
+namespace App\Tests\Application\DataTransformer\Apps\Body;
+
+use App\Application\DataTransformer\Apps\Body\BodyTagInsertedNewsDataTransformer;
+use Ec\Editorial\Domain\Model\Body\BodyTagHtml;
+use Ec\Editorial\Domain\Model\Body\BodyTagInsertedNews;
+use Ec\Editorial\Domain\Model\Editorial;
+use Ec\Editorial\Domain\Model\EditorialId;
+use Ec\Editorial\Domain\Model\EditorialTitles;
+use Ec\Editorial\Infrastructure\Client\Http\QueryEditorialClient;
+use Ec\Section\Domain\Model\QuerySectionClient;
+use Ec\Section\Domain\Model\Section;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Ken Serikawa <kserikawa@ext.elconfidencial.com>
+ */
+class BodyTagInsertedNewsDataTransformerTest extends TestCase
+{
+    private BodyTagInsertedNewsDataTransformer $transformer;
+
+    /** @var QueryEditorialClient|MockObject */
+    private QueryEditorialClient $queryEditorialClient;
+
+    /** @var QuerySectionClient|MockObject */
+    private QuerySectionClient $querySectionClient;
+
+    protected function setUp(): void
+    {
+        $this->queryEditorialClient = $this->createMock(QueryEditorialClient::class);
+        $this->querySectionClient = $this->createMock(QuerySectionClient::class);
+        $this->transformer = new BodyTagInsertedNewsDataTransformer(
+            $this->queryEditorialClient,
+            $this->querySectionClient,
+            'dev'
+        );
+    }
+
+    /**
+     * @test
+     *
+     * @param array $signatures
+     *
+     * @dataProvider \App\Tests\Application\DataTransformer\Apps\Body\DataProvider\BodyTagInsertedNewsDataProvider::getData()
+     */
+    public function transformBodyTagInsertedNewsWithSignatures(array $signatures): void
+    {
+        $resolveData = [];
+
+        $bodyElement = $this->createMock(BodyTagInsertedNews::class);
+        $bodyElement->method('editorialId')
+            ->willReturn($this->createMock(EditorialId::class));
+
+        $editorialMock = $this->createMock(Editorial::class);
+        $sectionMock = $this->createMock(Section::class);
+
+        $editorialMock->method('id')
+            ->willReturn($this->createMock(EditorialId::class));
+        $editorialMock->method('editorialTitles')
+            ->willReturn($this->createMock(EditorialTitles::class));
+        $editorialMock->method('publicationDate')
+            ->willReturn(new \DateTime());
+
+        $this->queryEditorialClient
+            ->expects($this->once())
+            ->method('findEditorialById')
+            ->willReturn($editorialMock);
+
+        $this->querySectionClient
+            ->expects($this->once())
+            ->method('findSectionById')
+            ->willReturn($sectionMock);
+
+        $resolveData['signatures'] = $signatures;
+
+        $result = $this->transformer->write($bodyElement, $resolveData)
+            ->read();
+
+
+
+        $this->assertArrayHasKey('editorialId', $result);
+        $this->assertIsString($result['editorialId']);
+        $this->assertArrayHasKey('title', $result);
+        $this->assertIsString($result['title']);
+        $this->assertArrayHasKey('signatures', $result);
+        $this->assertIsArray($result['signatures']);
+        $this->assertArrayHasKey('editorial', $result);
+        $this->assertIsString($result['editorial']);
+
+        // todo: validate photo index contents
+        $this->assertArrayHasKey('photo', $result);
+        $this->assertIsArray($result['photo']);
+    }
+
+    /**
+     * @test
+     */
+    public function canTransformShouldReturnBodyTagInsertedNewsString(): void
+    {
+        static::assertSame(BodyTagInsertedNews::class, $this->transformer->canTransform());
+    }
+}
---
+++ tests/Application/DataTransformer/Apps/Body/DataProvider/BodyTagInsertedNewsDataProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @copyright
+ */
+
+namespace App\Tests\Application\DataTransformer\Apps\Body\DataProvider;
+
+/**
+ * @author Ken Serikawa <kserikawa@ext.elconfidencial.com>
+ */
+class BodyTagInsertedNewsDataProvider
+{
+    public function getData(): array
+    {
+        return [
+            'no-signature' => [
+                []
+            ],
+            'one-signature' => [
+                [
+                    'journalistId' => 'journalistId',
+                    'aliasId' => 'aliasId',
+                    'name' => 'name',
+                    'url' => 'url',
+                    'departments' => [
+                        [
+                            'id' => 'id',
+                            'name' => 'name',
+                        ],
+                    ],
+                    'photo' => 'photo',
+                ],
+            ],
+            'two-signatures' => [
+                [
+                    'journalistId' => 'journalistId_1',
+                    'aliasId' => 'aliasId_1',
+                    'name' => 'name_1',
+                    'url' => 'url_1',
+                    'departments' => [
+                        [
+                            'id' => 'id',
+                            'name' => 'name',
+                        ],
+                    ],
+                    'photo' => 'photo_1',
+                ],
+                [
+                    'journalistId' => 'journalistId_2',
+                    'aliasId' => 'aliasId_2',
+                    'name' => 'name_2',
+                    'url' => 'url_2',
+                    'departments' => [
+                        [
+                            'id' => 'id',
+                            'name' => 'name',
+                        ],
+                    ],
+                    'photo' => 'photo_2',
+                ],
+            ],
+        ];
+    }
+}
