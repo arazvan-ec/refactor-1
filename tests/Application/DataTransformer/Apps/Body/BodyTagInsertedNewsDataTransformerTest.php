8db43c4b6685c7daab46cabf577ca1cabdab4f5c
Ken <kserikawa@ext.elconfidencial.com>
2024-11-22 11:19:08 +0100

    test: temp fix for BodyTagInsertedNews and Journalist data transformers

--- tests/Application/DataTransformer/Apps/Body/BodyTagInsertedNewsDataTransformerTest.php
+++ tests/Application/DataTransformer/Apps/Body/BodyTagInsertedNewsDataTransformerTest.php
@@ -14,6 +14,7 @@ use Ec\Editorial\Domain\Model\EditorialTitles;
 use Ec\Multimedia\Domain\Model\Clippings;
 use Ec\Multimedia\Domain\Model\Multimedia;
 use Ec\Section\Domain\Model\Section;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -22,6 +23,8 @@ use PHPUnit\Framework\TestCase;
 class BodyTagInsertedNewsDataTransformerTest extends TestCase
 {
     private BodyTagInsertedNewsDataTransformer $transformer;
+
+    private MockObject $thumbor;
 
     protected function setUp(): void
     {
