4ca7585be0795d6b01524c4460791ae5ed75d2ea
Razvan Alin Munteanu <arazvan@elconfidencial.com>
2024-12-18 12:03:49 +0100

    feat: add time-limit for worker

--- src/Infrastructure/Command/WorkerCommand.php
+++ src/Infrastructure/Command/WorkerCommand.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @copyright
+ */
+
 declare(strict_types=1);
 
 namespace App\Infrastructure\Command;
@@ -7,8 +11,12 @@ namespace App\Infrastructure\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * @author Razvan Alin Munteanu <arazvan@elconfidencial.com>
+ */
 #[AsCommand(name: 'app:worker', description: 'Worker')]
 class WorkerCommand extends Command
 {
@@ -20,11 +28,38 @@ class WorkerCommand extends Command
         $this->env = $env;
     }
 
+    public function configure(): void
+    {
+        $this
+            ->setName('app:worker')
+            ->addOption(
+                'time-limit',
+                't',
+                InputOption::VALUE_OPTIONAL,
+                'Execution time limit',
+                1
+            )
+            ->addOption(
+                'time-sleep',
+                's',
+                InputOption::VALUE_OPTIONAL,
+                'Execution time limit',
+                60
+            );
+    }
+
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        while ('dev' === $this->env) {
-            echo 'Test OK';
-            sleep(60);
+        $timeLimit = (int) $input->getOption('time-limit');
+        $timeSleep = (int) $input->getOption('time-sleep');
+
+        $executeStart = \time();
+        $executeEnd = $executeStart + $timeLimit;
+
+        while (('dev' === $this->env) && ((0 === $timeLimit) || ($executeEnd >= $executeStart))) {
+            $executeStart = \time();
+            $output->writeln((new \DateTime())->format('Y-m-d H:i:s').': Test OK');
+            sleep($timeSleep);
         }
 
         echo 'Test OK';
