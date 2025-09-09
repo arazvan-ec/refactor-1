bb98448117788545ab98a2527a8a35dd910ea506
lgomezec <lgomez@ext.elconfidencial.com>
2025-09-09 17:03:46 +0200

    feat: add new aspect ratios and sizes for multimedia handling

--- src/Application/DataTransformer/Apps/DetailsMultimediaMediaDataTransformer.php
+++ src/Application/DataTransformer/Apps/DetailsMultimediaMediaDataTransformer.php
@@ -35,6 +35,12 @@ class DetailsMultimediaMediaDataTransformer implements MultimediaMediaDataTransf
     /** @var string */
     private const ASPECT_RATIO_4_3 = '4:3';
 
+    /** @var string */
+    private const ASPECT_RATIO_3_2 = '3:2';
+
+    /** @var string */
+    private const ASPECT_RATIO_2_3 = '2:3';
+
     /** @var array<string, array<string, array<string, string> > > */
     private const SIZES_RELATIONS = [
         self::ASPECT_RATIO_4_3 => [
@@ -162,6 +168,110 @@ class DetailsMultimediaMediaDataTransformer implements MultimediaMediaDataTransf
             '360w' => [
                 self::WIDTH => '360',
                 self::HEIGHT => '480',
+            ],
+        ],
+        self::ASPECT_RATIO_3_2 => [
+            // High density
+            '1440w' => [
+                self::WIDTH => '1440',
+                self::HEIGHT => '960',
+            ],
+            '1200w' => [
+                self::WIDTH => '1200',
+                self::HEIGHT => '800',
+            ],
+            '996w' => [
+                self::WIDTH => '996',
+                self::HEIGHT => '664',
+            ],
+            // Desktop
+            '557w' => [
+                self::WIDTH => '557',
+                self::HEIGHT => '371',
+            ],
+            // Tablet
+            '381w' => [
+                self::WIDTH => '381',
+                self::HEIGHT => '254',
+            ],
+            // Mobile
+            '600w' => [
+                self::WIDTH => '600',
+                self::HEIGHT => '400',
+            ],
+            '414w' => [
+                self::WIDTH => '414',
+                self::HEIGHT => '276',
+            ],
+            '375w' => [
+                self::WIDTH => '375',
+                self::HEIGHT => '250',
+            ],
+            '360w' => [
+                self::WIDTH => '360',
+                self::HEIGHT => '240',
+            ],
+            // landscapePhotoFull
+            '767w' => [
+                self::WIDTH => '767',
+                self::HEIGHT => '511',
+            ],
+            // Low Quality Placeholder
+            'lo-res' => [
+                self::WIDTH => '48',
+                self::HEIGHT => '32',
+            ],
+        ],
+        self::ASPECT_RATIO_2_3 => [
+            // High density
+            '1440w' => [
+                self::WIDTH => '1440',
+                self::HEIGHT => '2160',
+            ],
+            '1200w' => [
+                self::WIDTH => '1200',
+                self::HEIGHT => '1800',
+            ],
+            '996w' => [
+                self::WIDTH => '996',
+                self::HEIGHT => '1494',
+            ],
+            // Desktop
+            '557w' => [
+                self::WIDTH => '557',
+                self::HEIGHT => '835',
+            ],
+            // Tablet
+            '381w' => [
+                self::WIDTH => '381',
+                self::HEIGHT => '571',
+            ],
+            // Mobile
+            '600w' => [
+                self::WIDTH => '600',
+                self::HEIGHT => '900',
+            ],
+            '414w' => [
+                self::WIDTH => '414',
+                self::HEIGHT => '621',
+            ],
+            '375w' => [
+                self::WIDTH => '375',
+                self::HEIGHT => '562',
+            ],
+            '360w' => [
+                self::WIDTH => '360',
+                self::HEIGHT => '540',
+            ],
+            // landscapePhotoFull
+            '767w' => [
+                self::WIDTH => '767',
+                self::HEIGHT => '1150',
+            ],
+            // Low Quality Placeholder
+            'lo-res' => [
+                self::WIDTH => '48',
+                self::HEIGHT => '72',
             ],
         ],
     ];
